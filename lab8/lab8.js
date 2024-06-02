6c539448390ec036212df47be3d87b5dbbcfa206
krepuk <repukekaterina@gmail.com>
2024-06-02 16:17:04 +0700

    вывод даты

--- lab8/lab8.js
+++ lab8/lab8.js
@@ -5,12 +5,12 @@ function showDate() {
     let out = document.getElementById('current-date');
     let today = new Date();
     out.innerHTML = `Дата и время для русской локали: ${today.toLocaleString('ru-RU')}<br>
-                    Дата и время для индийской локали: ${today.toLocaleString('mr-IN')}<br>                                 
-                    Дата и время для корейской локали: ${today.toLocaleString('ko-KR')}<br>         
-                    Дата и время для малазийской локали: ${today.toLocaleString('ms-MY')}<br>
-                    Дата и время для греческой локали: ${today.toLocaleString('el-GR')}<br>
+                    Дата и время для корейской локали: ${today.toLocaleString('ko-KR')}<br>                                 
+                    Дата и время для французской локали: ${today.toLocaleString('fr-LU')}<br>         
+                    Дата и время для испанской локали: ${today.toLocaleString('es-CL')}<br>
+                    Дата и время для австралийской локали: ${today.toLocaleString('en-AU')}<br>
                     Дата и время для американской локали: ${today.toLocaleString('en-US')}<br>
-                    Дата и время для египетской локали: ${today.toLocaleString('ar-EG')}<br>`;
+                    Дата и время для немецской локали: ${today.toLocaleString('de-DE')}<br>`;
 
     let date = document.getElementById('date');
     date.innerHTML = `Текущий день: ${today.getDate()}<br>
